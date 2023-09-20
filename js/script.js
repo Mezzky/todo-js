2afbde46307d25d5270ae2ab8e8933eea7b4cebe
Mezzky <ryansahadha@gmail.com>
2023-09-20 22:00:54 +0800

    Local Storage Berhasil

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ document.addEventListener('DOMContentLoaded', function () {
     event.preventDefault();
     addTodo();
   });
+  if (isStorageExist()) {
+    loadDataFromStorage();
+  }
 });
 
 function addTodo() {
@@ -168,3 +171,16 @@ function isStorageExist() /* boolean */ {
 document.addEventListener(SAVED_EVENT, function () {
   console.log(localStorage.getItem(STORAGE_KEY));
 });
+
+function loadDataFromStorage() {
+  const serializedData = localStorage.getItem(STORAGE_KEY);
+  let data = JSON.parse(serializedData);
+
+  if (data !== null) {
+    for (const todo of data) {
+      todos.push(todo);
+    }
+  }
+
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
